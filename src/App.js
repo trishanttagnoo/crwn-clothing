0c34ed35939958758943b9fdcc0a69db12f7c2f1
Trishant Tangnoo <trishant.tangnoo@tredence.com>
2022-01-03 14:50:13 +0530

    updating our app with a shop page, collection preview component and collection item component

--- src/App.js
+++ src/App.js
@@ -4,13 +4,14 @@ import React from 'react';
 // import { Switch,  Route, Router } from 'react-router-dom';
 import {Route, Routes, Switch} from "react-router-dom";
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
 
 // HatsPage Component for Routing Purpose
-const HatsPage = () =>(
-  <div>
-    <h1>HATS PAGE</h1>
-  </div>
-);
+// const HatsPage = () =>(
+//   <div>
+//     <h1>HATS PAGE</h1>
+//   </div>
+// );
 
 
 
@@ -21,7 +22,7 @@ function App() {
 
  <Switch>
     <Route  exact path="/" component={HomePage} />
-    <Route  path="/hats" component={HatsPage} />
+    <Route  path="/shop" component={ShopPage} />
 </Switch>
  
     </div>
