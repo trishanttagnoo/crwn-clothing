7d993d390ebd587b7a5e803cb058366ab89d1053
Trishant Tangnoo <trishant.tangnoo@tredence.com>
2022-01-03 10:22:04 +0530

    update our menu-items to navigate withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 import './App.css';
+import React from 'react';
+
+// import { Switch,  Route, Router } from 'react-router-dom';
+import {Route, Routes, Switch} from "react-router-dom";
 import HomePage from './pages/homepage/homepage.component';
+
+// HatsPage Component for Routing Purpose
+const HatsPage = () =>(
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+);
+
+
 
 function App() {
   return (
     <div>
-     <HomePage />
+     
+
+ <Switch>
+    <Route  exact path="/" component={HomePage} />
+    <Route  path="/hats" component={HatsPage} />
+</Switch>
+ 
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import {render} from 'react-dom';
+import {BrowserRouter} from 'react-router-dom';
 import './index.css';
 import App from './App';
+import HomePage from './pages/homepage/homepage.component';
 
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+
+render(
+  <BrowserRouter>
+   <App />
+  </BrowserRouter>,
+   document.getElementById('root')
 );
 
 
